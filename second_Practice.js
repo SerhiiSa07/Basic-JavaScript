ef852a4aa4f12a8407112071c09199095139df76
Serhii <875dfsb6435278785@gmail.com>
2022-12-28 20:47:49 +0100

    second_Practice

--- second_Practice.js
+++ second_Practice.js
@@ -8,15 +8,15 @@
 
 // ----  -- - -- Answer  - -- --------
 
-// const myDog = {
-//   // Only change code below this line
-// "name": "Friend",
-// "legs": 4,
-// "tails": 1,
-// "friends": ["Mouse", "Cat"]
+const myDog = {
+  // Only change code below this line
+  name: 'Friend',
+  legs: 4,
+  tails: 1,
+  friends: ['Mouse', 'Cat'],
 
-//   // Only change code above this line
-// };
+  //   // Only change code above this line
+};
 
 // --------------------------------assignments-------------
 
@@ -27,15 +27,15 @@
 // ----  -- - -- Answer  - -- --------
 
 // // Setup
-// const testObj = {
-//   "hat": "ballcap",
-//   "shirt": "jersey",
-//   "shoes": "cleats"
-// };
+const testObj = {
+  hat: 'ballcap',
+  shirt: 'jersey',
+  shoes: 'cleats',
+};
 
 // 3) Only change code below this line
-// const hatValue = testObj.hat;      // Change this line
-// const shirtValue = testObj.shirt;    // Change this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
 
 // --------------------------------assignments-------------
 
@@ -46,14 +46,14 @@
 // ----  -- - -- Answer  - -- --------
 
 // Setup
-// const testObj = {
-//   'an entree': 'hamburger',
-//   'my side': 'veggies',
-//   'the drink': 'water',
-// };
+const testObj = {
+  'an entree': 'hamburger',
+  'my side': 'veggies',
+  'the drink': 'water',
+};
 // // Only change code below this line
-// const entreeValue = testObj['an entree']; // Change this line
-// const drinkValue = testObj['the drink']; // Change this line
+const entreeValue = testObj['an entree']; // Change this line
+const drinkValue = testObj['the drink']; // Change this line
 
 // ------------------------assignments------------------
 
@@ -63,15 +63,15 @@
 // ------------------------ Answer  --------------------
 
 // Setup
-// const testObj = {
-//   12: "Namath",
-//   16: "Montana",
-//   19: "Unitas"
-// };
+const testObj = {
+  12: 'Namath',
+  16: 'Montana',
+  19: 'Unitas',
+};
 
 // // Only change code below this line
-// const playerNumber = 16;  // Change this line
-// const player = testObj[playerNumber];   // Change this line
+const playerNumber = 16; // Change this line
+const player = testObj[playerNumber]; // Change this line
 
 // ------------------------assignments------------------
 
@@ -81,15 +81,15 @@
 // ------------------------ Answer  --------------------
 
 // Setup
-// const myDog = {
-//   "name": "Coder",
-//   "legs": 4,
-//   "tails": 1,
-//   "friends": ["freeCodeCamp Campers"]
-// };
+const myDog = {
+  name: 'Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers'],
+};
 
 // Only change code below this line
-// myDog["name"] = "Happy Coder";
+myDog['name'] = 'Happy Coder';
 
 // ------------------------assignments------------------
 
@@ -98,14 +98,14 @@
 
 // ------------------------ Answer  --------------------
 
-// const myDog = {
-//   "name": "Happy Coder",
-//   "legs": 4,
-//   "tails": 1,
-//   "friends": ["freeCodeCamp Campers"]
-// };
+const myDog = {
+  name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers'],
+};
 
-// myDog["bark"] = "woof";
+myDog['bark'] = 'woof';
 
 // ------------------------assignments------------------
 
@@ -114,17 +114,17 @@
 // ------------------------ Answer  --------------------
 
 // Setup
-// const myDog = {
-//   "name": "Happy Coder",
-//   "legs": 4,
-//   "tails": 1,
-//   "friends": ["freeCodeCamp Campers"],
-//   "bark": "woof"
-// };
+const myDog = {
+  name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers'],
+  bark: 'woof',
+};
 
 // Only change code below this line
 
-// delete myDog["tails"];
+delete myDog['tails'];
 
 // ------------------------assignments---------------
 
@@ -135,25 +135,159 @@
 // ------------------------ Answer  --------------------
 
 // Setup
-// function phoneticLookup(val) {
-//   let result = "";
+function phoneticLookup(val) {
+  let result = '';
 
-//   // Only change code below this line
-//   const lookup = {
-//      "alpha": "Adams",
-//      "bravo": "Boston",
-//      "charlie": "Chicago",
-//      "delta": "Denver",
-//      "echo": "Easy",
-//      "foxtrot": "Frank"
-//   }
-//    result = lookup[val];
-//   // Only change code above this line
-//   return result;
-// }
+  // Only change code below this line
+  const lookup = {
+    alpha: 'Adams',
+    bravo: 'Boston',
+    charlie: 'Chicago',
+    delta: 'Denver',
+    echo: 'Easy',
+    foxtrot: 'Frank',
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
 
-// phoneticLookup("charlie");
+phoneticLookup('charlie');
 
 // ------------------------assignments------------------
 
-// 9)
+// 9) Измените функцию checkObj, чтобы проверить, содержит ли объект, переданный функции ( obj), определенное свойство ( checkProp). Если свойство найдено, верните значение этого свойства. Если нет, верните "Not Found".
+
+// ------------------------ Answer  --------------------
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  let myObj = {
+    gift: 'pony',
+    pet: 'kitten',
+    bed: 'sleigh',
+    city: 'Seattle',
+  };
+
+  return obj.hasOwnProperty(checkProp) ? myObj[checkProp] : 'Not Found';
+
+  return 'checkProp';
+
+  // Only change code above this line
+}
+
+// ------------------------assignments------------------
+
+// 10) Добавьте новый альбом в myMusic массив. Добавьте artistи title строки, release_year число и formats массив строк.
+
+// ------------------------ Answer  --------------------
+
+const myMusic = [
+  {
+    artist: 'Billy Joel',
+    title: 'Piano Man',
+    release_year: 1973,
+    formats: ['CD', '8T', 'LP'],
+    gold: true,
+  },
+  {
+    artist: 'Daft Punk',
+    title: 'Homework',
+    release_year: 1997,
+    formats: ['CD', 'Cassette', 'LP'],
+    gold: true,
+  },
+];
+
+// ------------------------assignments------------------
+
+// 11) Получите доступ к myStorage объекту и назначьте содержимое
+// glove box свойства gloveBoxContents переменной.
+// По возможности используйте запись через точку для всех свойств,
+// в противном случае используйте запись в квадратных скобках.
+
+// ------------------------ Answer  --------------------
+
+const myStorage = {
+  car: {
+    inside: {
+      'glove box': 'maps',
+      'passenger seat': 'crumbs',
+    },
+    outside: {
+      trunk: 'jack',
+    },
+  },
+};
+
+const gloveBoxContents = myStorage.car.inside['glove box'];
+
+// ------------------------assignments------------------
+
+// 12) Используя нотацию с точкой и квадратными скобками, установите переменную secondTreeна второй элемент в treesсписке от myPlantsобъекта.
+
+// ------------------------ Answer  --------------------
+
+const myPlants = [
+  {
+    type: 'flowers',
+    list: ['rose', 'tulip', 'dandelion'],
+  },
+  {
+    type: 'trees',
+    list: ['fir', 'pine', 'birch'],
+  },
+];
+
+const secondTree = myPlants[1].list[1];
+
+// ------------------------assignments------------------
+
+// 13) Вам дан объектный литерал, представляющий часть вашей коллекции музыкальных альбомов. Каждый альбом имеет уникальный идентификационный номер в качестве ключа и несколько других свойств. Не все альбомы имеют полную информацию.
+
+// Вы начинаете с updateRecords функции, которая принимает литерал объекта records, содержащий коллекцию музыкальных альбомов, id, а prop(like artist или tracks) и value. Завершите функцию, используя приведенные ниже правила, чтобы изменить объект, переданный функции.
+
+// Ваша функция всегда должна возвращать весь объект коллекции записей.
+// Если propнет tracks и value не является пустой строкой, обновите или установите для этого альбома prop значение value.
+// Если propе сть, tracks но в альбоме нет tracks свойства, создайте пустой массив и добавьте value в него.
+// Если prop это tracks и value не пустая строка, добавьте value ее в конец существующего tracks массива альбома.
+// Если value это пустая строка, удалите данное propсвойство из альбома.
+// Примечание.recordCollection Для тестов используется копия объекта.
+
+// ------------------------ Answer  --------------------
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name'],
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette'],
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: [],
+  },
+  5439: {
+    albumTitle: 'ABBA Gold',
+  },
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (value === '') {
+    delete records[id][prop];
+  } else if (prop === 'tracks') {
+    records[id][prop] ??= [];
+    records[id][prop].push(value);
+  } else {
+    records[id][prop] = value;
+  }
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
